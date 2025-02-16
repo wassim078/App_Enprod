ede9abe4fb52758c58045ae012462bad025b9fc2
wassim078 <wassim.jmili@esprit.tn>
2025-02-16 22:52:38 +0100

    Adding jwt authentication (when user log in)

--- src/Security/LoginFromAuthenticator.php
+++ src/Security/LoginFromAuthenticator.php
@@ -15,15 +15,19 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+
 
 class LoginFromAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
-
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    private JWTEncoderInterface $jwtEncoder;
+    public function __construct(private UrlGeneratorInterface $urlGenerator,JWTEncoderInterface $jwtEncoder)
     {
+        $this->urlGenerator = $urlGenerator;
+        $this->jwtEncoder = $jwtEncoder;
     }
 
     public function authenticate(Request $request): Passport
@@ -44,14 +48,52 @@ class LoginFromAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
+        $user = $token->getUser();
+
+        // Get the user's roles
+        
+
+        $jwtToken = $this->jwtEncoder->encode([
+            'username' => $user->getUserIdentifier(),
+            'roles' => $user->getRoles(),
+        ]);
+        $response = $this->createRedirectResponse($user);
+        $response->headers->setCookie(
+            new \Symfony\Component\HttpFoundation\Cookie(
+                "jwt_token", // Cookie name
+                $jwtToken,   // JWT token value
+                time() + 3600, // Expiration time (1 hour)
+                '/',        // Path
+                null,       // Domain
+                false,      // Secure (set to true if using HTTPS)
+                true        // HttpOnly
+            )
+        );
+        return $response;
+
+    }
+        private function createRedirectResponse($user): RedirectResponse
+    {
+        $roles = $user->getRoles();
+        // Redirect based on the user's role
+        if (in_array('ROLE_ADMIN', $roles, true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_templatebackoffice'));
+        } elseif (in_array('ROLE_CLIENT', $roles, true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_templatefrontoffice'));
+        } elseif (in_array('ROLE_LIVREUR', $roles, true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_templatefrontoffice'));
+        } elseif (in_array('ROLE_ENTREPRISE', $roles, true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_templatefrontoffice'));
         }
+
+        // Fallback route if no role matches
+        return new RedirectResponse($this->urlGenerator->generate('app_templatefrontoffice'));
+    }
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-    }
+       
+    
 
     protected function getLoginUrl(Request $request): string
     {
